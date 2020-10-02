23a8cb641034821fbc64b4f5f19aabe6346b2de6
Dariya Shakenova <shakenovadariya98@gmail.com>
2020-10-02 14:51:34 +0600

    Routing added

--- webapp/src/components/app/app.js
+++ webapp/src/components/app/app.js
@@ -1,4 +1,5 @@
 import  { HomePage, SignInPage, SignUpPage } from '../pages';
+import {Route, Switch} from 'react-router-dom';
 
 import Footer from '../footer/index';
 import Header from '../header/index';
@@ -8,7 +9,13 @@ const App = () => {
     return(
         <div className="flex flex-col items-center font-sans bg-purple-100">
             <Header />
-            <HomePage/>
+            <main role='main' >
+                <Switch>
+                    <Route path='/' exact component={HomePage} />
+                    <Route path='/signin' exact component={SignInPage} />
+                    <Route path='/signup' exact component={SignUpPage} />
+                </Switch>
+            </main>
             <Footer />
         </div>
     )
--- webapp/src/components/header/header.js
+++ webapp/src/components/header/header.js
@@ -1,5 +1,6 @@
 import './header.css'
 
+import {Link} from 'react-router-dom';
 import React from 'react';
 
 const Header = () => {
@@ -7,10 +8,16 @@ const Header = () => {
         <div className="bg-purple-600 w-full px-4 py-2 flex justify-center">
             <div className="max-w-3xl w-full ">
                 <div className="flex items-center justify-between text-purple-200">
-                    <div className="text-purple-900 font-bold text-lg font-mono hover:text-purple-800 cursor-pointer select-none">EasySubmit</div>
+                    <Link to='/'>
+                        <div className="text-purple-900 font-bold text-lg font-mono hover:text-purple-800 cursor-pointer select-none">EasySubmit</div>
+                    </Link>
                     <div className="flex">
-                        <button type="button" className="border-2 border-purple-200 rounded-lg  text-xs mr-2 font-thin p-1 ">Sign in</button>
-                        <button type="button" className="border-2 border-purple-200 rounded-lg  text-xs font-thin p-1">Sign up</button>
+                        <Link to="/signin">
+                            <button type="button" className="border-2 border-purple-200 rounded-lg  text-xs mr-2 font-thin p-1 ">Sign in</button>
+                        </Link>
+                        <Link to='/signup'>
+                            <button type="button" className="border-2 border-purple-200 rounded-lg  text-xs font-thin p-1">Sign up</button>                    
+                        </Link>
                     </div>
                 </div>
             </div>
