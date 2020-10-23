15eb6a2ead067d2390af8f690c4a17953599ebad
Highzest <epmek96@gmail.com>
2020-10-23 15:02:10 +0600

    Added serving web app

--- api/app/router/router.go
+++ api/app/router/router.go
@@ -1,6 +1,9 @@
 package router
 
 import (
+	"net/http"
+	"os"
+
 	"github.com/go-chi/chi"
 
 	"api/app/requestlog"
@@ -8,6 +11,7 @@ import (
 	"api/app/server"
 )
 
+// New is a function to create the main router.
 func New(s *server.Server) *chi.Mux {
 	l := s.Logger()
 
@@ -25,9 +29,25 @@ func New(s *server.Server) *chi.Mux {
 		r.Method("DELETE", "/homework/{id}", requestlog.NewHandler(s.HandleDeleteHomework, l))
 	})
 
+	FileServer(r)
+
 	// Routes for healthz
 	r.Get("/healthz/liveness", server.HandleLive)
 	r.Method("GET", "/healthz/readiness", requestlog.NewHandler(s.HandleReady, l))
 
 	return r
+}
+
+// FileServer is serving static files.
+func FileServer(router *chi.Mux) {
+	root := "./web"
+	fs := http.FileServer(http.Dir(root))
+
+	router.Get("/*", func(w http.ResponseWriter, r *http.Request) {
+		if _, err := os.Stat(root + r.RequestURI); os.IsNotExist(err) {
+			http.StripPrefix(r.RequestURI, fs).ServeHTTP(w, r)
+		} else {
+			fs.ServeHTTP(w, r)
+		}
+	})
 }
