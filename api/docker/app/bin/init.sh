56b5cd91a49a30288bd97d68e32ea9dcb331e85d
Highzest <epmek96@gmail.com>
2020-11-28 21:02:17 +0600

    Removing docker image for app, since it was redundant

--- api/docker/app/bin/init.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-echo "Running migrations..."
-/api/migrate up > /dev/null 2>&1 &
-
-echo 'Deleting mysql-client...'
-apk del mysql-client
-
-echo 'Starting application...'
-/api/app
