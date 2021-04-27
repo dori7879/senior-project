0178610dca690e9fae1d98c54109d79128d22355
Yermek M <epmek96@gmail.com>
2021-04-27 15:21:45 +0600

    Added NODE_ENV env var to Heroku

--- webapp/src/services/index.js
+++ webapp/src/services/index.js
@@ -1,1 +1,1 @@
-export const BASE_API_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:8080' : '' 
+export const BASE_API_URL = process.env.NODE_ENV === 'production' ? '' : 'http://localhost:8080'
